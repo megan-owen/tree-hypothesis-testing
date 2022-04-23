1cce58598649c30d2c4e8979354872c3bf74d179
Devastating-Lee <78444693+Devastating-Lee@users.noreply.github.com>
2022-04-22 22:22:27 -0400

    Add files via upload
    Updated the file so that the function panda_plus_symmetry takes in 2 parameters a data frame and an integer for the size of the matrix that will be generated (this is just the number of trees that will be passed in) the function also now outputs a dataframe

--- AD_Package/panda_plus_symmetry.py
+++ AD_Package/panda_plus_symmetry.py
@@ -4,13 +4,13 @@ import pandas as pd
 # pd.set_option('display.max_columns', None)  # forces panda to display all the columns
 # pd.set_option('display.max_rows', None)
 
-distances_unfiltered = pd.read_csv("Sim_Trees_distances_comparisons.csv")  # loads in data frame
+
 
 filterArray = []  # array for all the tree filters that will be generated
 
 dataframeSetup = []  # array for the index and column names
 
-final_Num_Trees = 40  # VERY IMPORTANT : this will be the value that is shared through the program the number of trees
+
 
 
 # this is the total number of trees that we will take the weighted distances from (This is basically the only thing
@@ -21,15 +21,18 @@ def data_frame_setup(numTrees):
         dataframeSetup.append("Tree" + str(p + 1))  # add the word tree# +1 to the data frame set up (start a tree1)
 
 
-def panda_plus_symmetry_generate(): #function for calling from master file
+def panda_plus_symmetry_generate(sim_tree_distancesDF, final_Num_Trees): #function for calling from master file takes in A DATAFRAME with the tree comparison distances (sim_tree_distancesDF) and the rows x columns of the matrix you want to create (final_Num_trees)
+
+    distances_unfiltered = sim_tree_distancesDF  #  Note: Expects a Dataframe this loads in data frame
+    final_Num_Trees = final_Num_Trees  # VERY IMPORTANT : this will be the value that is shared through the program the number of trees (same as num of rows and columns)
+
     data_frame_setup(final_Num_Trees)  # create the dataframe row and column names and put them in the array called
     # data_frame_setup
     weightDataFrame = pd.DataFrame(columns=dataframeSetup, index=dataframeSetup)
     # set an empty data frame with 40 columns and 40 rows
 
     for i in range(final_Num_Trees):  # create the filters a put them into a filter array
-        tree_filter = (distances_unfiltered["Num_First_tree"] == i) & (distances_unfiltered[
-                                                                           "Num_Second_tree"] <= final_Num_Trees)  # tree filter is collection of boolean values literally a filter filters the trees into boolean values
+        tree_filter = (distances_unfiltered["Num_First_tree"] == i) & (distances_unfiltered["Num_Second_tree"] <= final_Num_Trees)  # tree filter is collection of boolean values literally a filter filters the trees into boolean values
         filterArray.append(tree_filter)
 
     for i in range(final_Num_Trees):
@@ -74,5 +77,6 @@ def panda_plus_symmetry_generate(): #function for calling from master file
 
     print(weightDataFrame)  # print out the dataframe
 
-    weightDataFrame.to_csv("symmetricMatrix.csv")
+    return weightDataFrame
+    # weightDataFrame.to_csv("symmetricMatrix.csv")
 
